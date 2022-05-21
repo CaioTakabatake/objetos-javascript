8b19e573088f34b2b8b267df6af6d8a80b963e05
Caio <caio.soldera.taka@gmail.com>
2022-05-21 19:05:26 -0300

    definindo objetos

---
+++ aula-01/objetos.js
@@ -0,0 +1,12 @@
+const listaCPFs = ["12365242625", "9242624265", "9352524652"];
+
+// const cliente = ['nome', 'Andre', 'idade', 36];
+
+const cliente = {
+    nome: 'Andre',
+    idade: 36,
+    cpf: "12543652266",
+    email: "andre@email.com"
+}
+
+console.log(cliente);
