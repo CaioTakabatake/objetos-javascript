8afe055a5f1f7595cdb24c7a5f45a2d389ca9891
Caio <caio.soldera.taka@gmail.com>
2022-05-21 20:31:40 -0300

    objetos em objetos

---
+++ aula-02/compondo-objetos.js
@@ -0,0 +1,20 @@
+const cliente = {
+    nome: 'Andre',
+    idade: 36,
+    cpf: "12543652266",
+    email: "andre@email.com",
+    fones: [
+        '5591235498',
+        '5521988743124'
+    ]
+}
+
+cliente.dependentes = {
+    nome: 'Sara',
+    parentesco: 'filha',
+    dataNascimento: '20/03/2011'
+}
+console.log(cliente);
+
+cliente.dependentes.nome = 'Sara Silva';
+console.log(cliente);
